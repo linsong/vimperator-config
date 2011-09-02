9c478b9acd5af0fd5c2e5a370d356479f3881148
Vincent Wang <linsong.qizi@gmail.com>
2011-09-02 12:19:37 +0800

    add a simple plugin for pleac access

---
+++ plugin/pleac.js
@@ -0,0 +1,28 @@
+function Pleac() {
+  return {
+    _execute: function(args){
+      var lang = args.length ? args.shift() : 'python';
+      var langs = ['perl', 'python', 'ruby', 'c++', 'bash', 'haskell', 'java', 'objective-c'];
+      if (langs.indexOf(lang) == -1) {
+        liberator.echoerr('Unsupported pleac command: ' + lang);
+        return false;
+      }
+      var part = args.length ? args.shift() : 'index';
+      var url = "http://pleac.sourceforge.net/pleac_"+lang+"/"+part+".html";
+      liberator.execute(":open  "+url);
+    },
+
+    _completer: function(context) {
+      completions = ['perl', 'python', 'ruby', 'c++', 'bash', 'haskell', 'java', 'objective-c', "strings", "numbers", "datesandtimes", "arrays", "hashes", "patternmatching", "fileaccess", "filecontents", "directories", "subroutines", "referencesandrecords", "packagesetc", "classesetc", "dbaccess", "userinterfaces", "processmanagementetc", "sockets", "internetservices", "cgiprogramming", "webautomation"];
+      context.completions = [[c, ''] for each (c in completions)];
+    }
+  };
+};
+
+var _pleac = new Pleac();
+
+commands.add(['pleac'],
+  'open pleac page for given language',
+  function(args) { _pleac._execute(args); },
+  { argCount: '*', completer: _pleac._completer }
+);
